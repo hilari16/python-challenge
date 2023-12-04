dff25ec53f61f2fbe7c4d5b5a26070f04ae07529
hilari16 <hilari.woodard@yahoo.com>
2023-12-04 15:52:31 -0600

    PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,16 +2,12 @@ import os
 import csv
 
 budget_csv = os.path.join('Resources','budget_data.csv')
+output_file = os.path.join('analysis','budget_analysis.txt')
 
 #lists to store data
 date = []
 net_profit = []
 avg_profit = []
-
-print("Financial Analysis")
-print()
-print("-------------------------------")
-print()
 
 with open(budget_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -25,18 +21,13 @@ with open(budget_csv) as csvfile:
         # Add net_profit
         net_profit.append(row[1])
         
-    
-    print(f"Total Months: ", len(date))
-    print()
-    
+      
     # make net_profit an integer
     net_profit_int = [eval(i) for i in net_profit]
 
     # sum net_profit_int
     profit_sum = sum(net_profit_int)
-    print(f"Total: $", profit_sum)
-    print()
-
+   
     #solving for the average change in profit/losses
     # [(row2-row1)+(row3-row2)+...+(row87-row86)]/85
     for i in range(len(net_profit_int)-1):
@@ -44,23 +35,29 @@ with open(budget_csv) as csvfile:
         avg_profit.append(change_in_profit)
     
     change_in_profit_avg = sum(avg_profit)/len(avg_profit)
-    print(f"Average Change: $", round(change_in_profit_avg,2))
-    print()
-
+   
     #find the greatest increase in profit
     max_profit = max(avg_profit)
     date_max_profit = date[(avg_profit.index(max_profit))+1]
-    print(f"Greatest Increase in Profits:",date_max_profit,"($",max_profit,")")
-    print()
-
+  
     #find the greatest decrease in profit
     min_profit = min(avg_profit)
     date_min_profit = date[(avg_profit.index(min_profit))+1]
-    print(f"Greatest Decrease in Profits: ", date_min_profit, "($",min_profit,")")
-
-
+    
+output = (
+    f"Financial Analysis\n"
+    f"-------------------------------\n"
+    f"Total Months: {len(date)}\n"
+    f"-------------------------------\n"
+    f"Total: ${profit_sum}\n"
+    f"Average Change: ${round(change_in_profit_avg,2)}\n"
+    f"Greatest Increase in Profits: {date_max_profit} (${max_profit})\n"
+    f"Greatest Decrease in Profits: {date_min_profit} (${min_profit})\n"
+)
    
-
+with open(output_file,"w") as txtfile:
+    txtfile.write(output)
+    print(output)
 
 
     
